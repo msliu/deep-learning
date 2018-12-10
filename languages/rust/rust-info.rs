c97a47c90e9c301bc761f1fab0bebaab406f5854
msliu <vincent.liu1013@gmail.com>
2018-12-10 21:23:59 +0800

    update language info

---
+++ languages/rust/rust-info.rs
@@ -0,0 +1,61 @@
+
+
+   1. What is &'a in Rust Language [duplicate]
+      https://stackoverflow.com/questions/47640550/what-is-a-in-rust-language
+         A shared reference type is written &type, or &'a type when you need to specify an explicit lifetime.
+
+   2. What is the meaning of &' in Rust?
+      https://www.quora.com/What-is-the-meaning-of-in-Rust
+
+   3. Confusion : Struct, impl, self, trait
+      https://users.rust-lang.org/t/confusion-struct-impl-self-trait/3941
+
+   4. Traits
+      https://doc.rust-lang.org/rust-by-example/trait.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+slide 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
