1735c68eb0700b9e9e7fb57ff39cc32ffd1b6cfa
vincentliu <vincentliu@emotibot.com>
2018-12-22 17:47:05 +0800

    updat papers info

---
+++ papers/segmentation-study/unet-study.cpp
@@ -0,0 +1,13 @@
+Learn How to Train U-Net On Your Dataset
+https://medium.com/coinmonks/learn-how-to-train-u-net-on-your-dataset-8e3f89fbd623
+
+U-Net: Convolutional Networks for Biomedical Image Segmentation
+https://www.renom.jp/notebooks/tutorial/image_processing/u-net/notebook.html
+
+A Multi-scale U-Net for Semantic Segmentation of Histological Images from Radical Prostatectomies
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5977596/
+
+
+
+
+
