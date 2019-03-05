b509ff59e12fc46b3145a84e15a4ab04b1d2183c
msliu <vincent.liu1013@gmail.com>
2019-03-05 09:06:50 +0800

    update info

--- image-topic/semantic-segmentation/unet-study.cpp
+++ image-topic/semantic-segmentation/unet-study.cpp
@@ -10,4 +10,7 @@ https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5977596/
 Lesson 14: Deep Learning Part 2 2018 - Super resolution; Image segmentation with Unet
 https://www.youtube.com/watch?v=nG3tT31nPmQ
 
-
+topic: BN with unet
+Normalization  in  Training  U-Net  for  2D  Biomedical  SemanticSegmentation
+https://arxiv.org/pdf/1809.03783.pdf
+https://github.com/XiaoYunZhou27/UNet-with-BN-GN-IN-LN
