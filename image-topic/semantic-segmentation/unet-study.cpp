0809c6caeca29594d74e2edb9dbd8af10635211c
vincentliu <vincentliu@emotibot.com>
2019-03-06 09:59:35 +0800

    update info

--- image-topic/semantic-segmentation/unet-study.cpp
+++ image-topic/semantic-segmentation/unet-study.cpp
@@ -10,6 +10,18 @@ https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5977596/
 Lesson 14: Deep Learning Part 2 2018 - Super resolution; Image segmentation with Unet
 https://www.youtube.com/watch?v=nG3tT31nPmQ
 
+Semantic Segmentation with Deep Learning
+https://towardsdatascience.com/semantic-segmentation-with-deep-learning-a-guide-and-code-e52fc8958823?fbclid=IwAR2kz5Ji1-cPUd7UgllRGQJzjjHU6ITVCVqhyw3uEiSuTd5K7zwmUCPoRuU
+ 
+
+Conditional Random Fields: Probabilistic Modelsfor Segmenting and Labeling Sequence Data
+https://repository.upenn.edu/cgi/viewcontent.cgi?article=1162&context=cis_papers
+
+
+Good fb discussion for u-net
+https://www.facebook.com/groups/2027602154187130/permalink/2391039427843399/
+
+
 topic: BN with unet
 Normalization  in  Training  U-Net  for  2D  Biomedical  SemanticSegmentation
 https://arxiv.org/pdf/1809.03783.pdf
